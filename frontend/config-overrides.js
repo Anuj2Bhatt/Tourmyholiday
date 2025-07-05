80b42de0e6aedb44c4cae50fa24c0562b53a3c99
Anujbhatt <anujbhatt479@gmail.com>
2025-07-05 12:30:49 +0530

    Delete frontend directory

--- frontend/config-overrides.js
+++
@@ -1,5 +0,0 @@
-// Simple config-overrides.js file without customize-cra
-module.exports = function override(config, env) {
-  // Add any custom webpack configuration here if needed
-  return config;
-}; 
