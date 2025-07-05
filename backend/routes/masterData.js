629ce13c49f1233bd381ac1d5e64c9dfbc1b5087
Anujbhatt <anujbhatt479@gmail.com>
2025-07-05 12:30:11 +0530

    Delete backend directory

--- backend/routes/masterData.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const masterDataController = require('../controllers/masterDataController');
-
-// Get all master data
-router.get('/', masterDataController.getAllMasterData);
-
-module.exports = router; 
